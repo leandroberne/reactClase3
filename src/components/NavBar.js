0213610b75b858beba36c657bc9ac22a61a2edb5
Leandro Berne <leandro@plw.com.ar>
2021-08-05 01:08:44 -0300

    primer commit navbar clase3

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+const NavBar = () => {
+    return(
+        <nav>
+            <ul>
+                <li class="brand">JAMIROQUAI STORE</li>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Productos</a></li>
+                <li><a href="#">Nosotros</a></li>
+                <li><a href="#">Contacto</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default NavBar;
